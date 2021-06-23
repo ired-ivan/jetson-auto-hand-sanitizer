bca42c0479c84d6c6114aa443d9d4a578cb599ab
ired-ivan <ivanto@ired.com.hk>
2021-06-22 21:13:09 -0700

    20210623-update

--- file/motor.sh
+++ file/motor.sh
@@ -4,3 +4,4 @@ sudo usermod -aG i2c jetbot
 sudo groupadd -f -r gpio
 sudo usermod -a -G gpio jetbot
 sudo udevadm control --reload-rules && sudo udevadm trigger
+sudo cp ~/jetson-auto-hand-sanitizer/file/camera.py /usr/local/lib/python3.6/dist-packages/jetbot-0.4.0-py3.6.egg/jetbot/camera.py
